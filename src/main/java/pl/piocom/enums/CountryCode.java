e01dda6b8bf68ca12727ffcf0de6cb56e0adcd99
Piotr <piotrciolczyk992@gmail.com>
2021-12-14 21:09:36 +0100

    Added values and contructor in enum class. Small changes with "location-add.html" view. (allCodes view)

--- src/main/java/pl/piocom/enums/CountryCode.java
+++ src/main/java/pl/piocom/enums/CountryCode.java
@@ -1,6 +1,21 @@
 package pl.piocom.enums;
 
 public enum CountryCode {
-    PL, CZ, SK, HU, DE, BE, NL, FR, ES;
+    //Those are only countries used by our freightexchange.
+    // Codes are written by code type "ISO 3166-1 alpha-3 codes"
+    POL("Poland"),
+    CZE("Czechia"),
+    SLO("Slovakia"),
+    HUN("Hungary"),
+    GER("Germany"),
+    BEL("Belgium"),
+    NLD("Netherlands"),
+    FRA("France"),
+    ESP("Spain");
 
+    public final String label;
+
+    private CountryCode(String label) {
+        this.label = label;
+    }
 }
